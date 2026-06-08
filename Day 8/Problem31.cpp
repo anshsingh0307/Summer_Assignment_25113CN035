95e61600d91fb0997bcfcae6769e4a20cc20155f
Ansh Singh <singhanshak57@gmail.com>
2026-06-08 20:37:00 +0530

    08/06/2026

---
+++ Day 8/Problem31.cpp
@@ -0,0 +1,24 @@
+/* Write a program to Print character triangle.
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+#include <iostream>
+using namespace std ;
+
+int main() {
+    int n ;
+    cout << "Enter the number of rows : " ;
+    cin >>n ;
+    for(int i=1 ; i<=n ;i++){
+        char ch = 'A';
+        for (int j=1 ; j<=i ; j++){
+            cout << ch ;
+                ch++ ;
+            }
+       cout << endl ;
+    }
+    return 0 ;
+}
