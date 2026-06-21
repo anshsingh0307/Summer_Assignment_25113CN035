b905614c97f9531138cb6e3d3b4ed707db3be12f
Ansh Singh <singhanshak57@gmail.com>
2026-06-21 22:49:50 +0530

    20/06/2026

---
+++ Day_20/Question78.cpp
@@ -0,0 +1,32 @@
+//Write a program to Check symmetric matrix. 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter order of matrix: ";
+    cin >> n;
+    int a[n][n];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> a[i][j];
+        }
+    }
+    bool symmetric = true;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(a[i][j] != a[j][i]) {
+                symmetric = false;
+                break;
+            }
+        }
+    }
+    if(symmetric)
+        cout << "Matrix is symmetric";
+    else
+        cout << "Matrix is not symmetric";
+    return 0;
+}
---
+++ Day_20/Question80.cpp
@@ -0,0 +1,28 @@
+// Write a program to Find column-wise sum.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter number of rows and columns: ";
+    cin >> rows >> cols;
+    int a[rows][cols];
+
+    cout << "Enter matrix elements:\n";
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            cin >> a[i][j];
+        }
+    }
+    cout << "Column-wise sums:\n";
+    for(int j = 0; j < cols; j++) {
+        int sum = 0;
+        for(int i = 0; i < rows; i++) {
+            sum += a[i][j];
+        }
+
+        cout << "Sum of column " << j + 1 << " = " << sum << endl;
+    }
+    return 0;
+}
